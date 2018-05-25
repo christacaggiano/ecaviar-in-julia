8cacede908c82cde84f1789fd831a0df020e4900
Christa.Caggiano <christacaggiano@gmail.com>
2018-05-24 23:31:43 -0700

    intial commit

---
+++ ecaviar_redo.jl
@@ -0,0 +1,18 @@
+
+function read_files()
+
+
+
+
+function main()
+
+  # @TODO take in command line arguments
+  non_centrality_parameter = 5.7  # defined in paper?
+  rho = 0
+  casual_snp_number = 1
+
+  # file names
+  gwas_LD = "LDgwas.txt"
+  eqtl_LD = "LDeqtl.txt"
+
+end
